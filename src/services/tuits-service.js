f2c80ec1eb04e3998d58957dc2960fe38f028dfb
CuichanWu <wu.cu@northeastern.edu>
2023-04-04 15:31:43 -0700

    trigger

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -4,6 +4,7 @@ import axios from "axios";
 // const TUITS_API =
 // 	"https://tuiter-node-server-app-kq1j.onrender.com/api/tuits";
 
+
 const API_BASE = process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits`;
 
